f6f024cbc662964cb80b23e4677290d1c0517e8a
Süleyman Sevimli <suleyman.sevimli1@gmail.com>
2021-08-28 16:00:10 +0300

    added storybook for component documentation

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Getir Tailwind',
+  brandUrl: 'https://getir-react-tailwind.netlify.app/',
+  brandImage: 'https://pbs.twimg.com/profile_images/1353420928596783104/-WsDVf6R_400x400.jpg',
+});
--- src/components/ui/CategoryItem.js
+++ src/components/ui/CategoryItem.js
@@ -1,10 +1,21 @@
+import PropTypes from 'prop-types';
+
+/**
+ * ### CategoryItem
+ * @param {*} card: Object `{url: String | null, image: String, title: String}` 
+ * @returns JSX Element
+ */
 function CategoryItem({ category }) {
 	return (
-        <a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+        <a href={ category.url ?? "#" } className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
             <img src={category.image} className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
             <span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">{category.title}</span>
         </a>
 	)
 }
 
-export default CategoryItem
+export default CategoryItem;
+
+CategoryItem.propTypes = {
+    category: PropTypes.object
+}
